d0ad500c7ec4d6fbe5433bc70f28d73f2367a793
Cameron Francis <cpfrancis1@gmail.com>
2023-09-25 07:04:28 +1000

    stream downloader commit 3

--- download_stream.py
+++ download_stream.py
@@ -6,6 +6,8 @@ Improvements:
 - make sure to clean up files after stitching together
 - host this library on github
 - Write seperate program that will add an intro and outro video automatically
+- Monitoring and Logging:
+- options to choose output format
 
 Terms: 
 HLS = HTTP Live streeaming
@@ -45,6 +47,7 @@ import aiohttp
 import subprocess
 import os
 import re
+import tempfile
 from datetime import datetime, timedelta
 
 
@@ -57,13 +60,59 @@ def get_m3u8_media_playlist(m3u8_url):
         print(f'Error Text. {response.text}')
         return None
     
-def get_segment_duration(m3u8_media_playlist):
+def get_stream_duration(m3u8_media_playlist):
     segment_duration_pattern = r'#EXT-X-TWITCH-TOTAL-SECS:(\d+.+)[^\n]*'
     segment_duration = re.findall(segment_duration_pattern, m3u8_media_playlist)
     return segment_duration
     # stream_duration = r''
     # not sure if i need to pass the file for the file name
 
+def get_segment_duration(m3u8_media_playlist):
+    stream_duration_pattern = r'#EXT-X-TARGETDURATION:(\d+.+)[^\n]*'
+    stream_duration = re.findall(stream_duration_pattern, m3u8_media_playlist)
+    return stream_duration
+
+def get_base_url(url):
+    pattern = r'(.*/)'
+    baseurl = re.findall(pattern, url)
+    return baseurl
+
+"""
+- calculate the chunks to be downloaded
+    - start time seconds/segment duration = first chunk
+    - end time second/segment duration = last chunk
+"""
+def get_chunk_list(base_url, start_time_seconds, end_time_seconds, segment_duration):
+    start_segment = start_time_seconds/segment_duration
+    end_segment = end_time_seconds/segment_duration 
+    total_segments = end_segment - start_segment
+    Url_list = []
+    for i in total_segments:
+        string = f"{base_url}{i}.ts"
+        Url_list.append(string)
+    return Url_list
+
+
+def download_chunks(chunk_list, temp_dir):
+    local_filename = os.path.join(temp_dir, url.split('/')[-1])
+    with open(local_filename, 'wb') as local_chunk:
+        for url in chunk_list:
+            print(f"Downloading chunk @: {url}")
+            chunk = requests.get(url)
+            if chunk:
+                local_chunk.write(chunk)
+                print(f"Downloaded segment {local_filename}")
+
+def concat_segments(temp_dir, title):
+    
+
+
+
+# def check_if_within_video(stream_duration, end_time):
+#     end_time_seconds = time_to_seconds(end_time)
+#     if end_time_seconds < stream_duration
+        
+#     else:
 
 
 def time_to_seconds(time_str):
--- test.py
+++ test.py
@@ -1,10 +1,30 @@
-from download_stream import get_segment_duration, get_m3u8_media_playlist
+from download_stream import get_segment_duration, get_stream_duration, get_m3u8_media_playlist, time_to_seconds, get_chunk_list, get_base_url, download_chunks, concat_segments
+import tempfile
+ # Enter time in format HH:MM:SS
 
 def main():
+    url = "https://d1m7jfoe9zdc1j.cloudfront.net/ac905f0ca491b7eea5a6_chickchau_41847695977_1694646588/chunked/index-dvr.m3u8"
+    start_time = "05:42:12" # Enter time in format HH:MM:SS
+    end_time = "05:44:12"
+    title = "Test Concat"
     with open('index-dvr.m3u8', 'r') as file:
         data = file.read()
-    test = get_segment_duration(data)
-    print(test[0])
+    segment_duration = get_segment_duration(data)
+    stream_duration = get_stream_duration(data)
+    print(segment_duration)
+    print(stream_duration)
+    start_time_seconds = time_to_seconds(start_time)
+    end_time_seconds = time_to_seconds(end_time)
+    if time_to_seconds(end_time) < stream_duration:
+        baseurl = get_base_url(url)
+        chunk_list = get_chunk_list(baseurl, start_time_seconds, end_time_seconds, segment_duration)
+        temp_dir = tempfile.mkdtemp()
+        download_chunks(chunk_list, temp_dir)
+        concat_segments()
+
+
+
+    
 
 main()
 
